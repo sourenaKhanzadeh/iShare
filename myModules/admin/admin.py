3880d1389c99fc4abcc5b87dffa6584c345647eb
Sourena Khanzadeh <soure@DESKTOP-6PVNFEF.(none)>
2019-05-22 09:59:18 -0400

    admin query more efficient

--- myModules/admin/admin.py
+++ myModules/admin/admin.py
@@ -3,10 +3,10 @@ from myModules.model.database import repos
 from flask import session, redirect, flash
 from myModules.tools.tools import millify
 
-def queryAll():
+def queryAll(type):
     # get all the repos
     # sort by star in descending order
-    query = repos.find().sort('star', -1)
+    query = repos.find({'pending':type}).sort('star', -1)
     queries = []
 
     # query all the repos
@@ -22,19 +22,17 @@ def queryAll():
 
 @app.route('/<user>/admin', methods=['GET', 'POST'])
 def admin(user):
+    # get all queries
+    queries = queryAll(True)
+    
     # admin is judging the post
     if request.method == "GET":
 
-        # get all queries
-        queries = queryAll()
 
         # render all time
         return render_template('pages/admin.html', queries=queries)
     # admin made a decision
     else:
-
-        # get all queries
-        queries = queryAll()
 
         # take admin decision
         decision = bool(request.form['approve'])
