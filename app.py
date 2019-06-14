07146d586b01b55281c8039b860093c838ba015a
Sourena Khanzadeh <soure@DESKTOP-6PVNFEF.(none)>
2019-06-14 15:42:48 -0400

    bugs fixes

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import render_template, request, session, redirect, flash, url_for
 from flask import Flask, jsonify
+import os
 
 app = Flask(__name__)
 # app.debug = True
@@ -11,5 +12,5 @@ app.config['SECRET_KEY'] = 'ImAPrettyBird'
 from views import *
 
 if __name__ == '__main__':
-    # socketio.run(app)
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
