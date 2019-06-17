dd442cf0d2a2e5f57976ff03901294d9945b7c6a
Sourena Khanzadeh <soure@DESKTOP-6PVNFEF.(none)>
2019-06-17 11:52:38 -0400

    enhanced comments

--- myModules/paper_manager/detail.py
+++ myModules/paper_manager/detail.py
@@ -1,4 +1,4 @@
-from app import app, render_template, session, request,jsonify
+from app import app, render_template, session, request,jsonify, flash
 from myModules.model.database.database import global_database, ids, comments
 
 
@@ -24,42 +24,46 @@ def detail(user, title):
     # make star human readable
     repo['star'] = "{:,}".format(repo['star'])
 
-    # user made comments
-    if request.args.get('user') is not None:
+    # check if user is signed in
+    if session.get('username') is not None:
+        # user made comments
+        if request.args.get('user') is not None:
 
-        # if comment section exist
-        if global_database.find_one(ids['comments'],
-                    title=title
-            ) is not None:
-            # get data
-            content = {
-                'username':request.args.get('user'),
-                'date':request.args.get('today'),
-                'comment':request.args.get('comment')
-            }
-            # then update the comment section
-            comments.update({'title':title},
-                            {
-                            '$push':{
-                                'comments':content
-                            }
-            })
-            # send to ajax
-            return jsonify(content)
-        else:
-            # get data
-            content = {
+            # if comment section exist
+            if global_database.find_one(ids['comments'],
+                        title=title
+                ) is not None:
+                # get data
+                content = {
                     'username':request.args.get('user'),
                     'date':request.args.get('today'),
                     'comment':request.args.get('comment')
                 }
-            # insert to the first comment to the database
-            global_database.insert(ids['comments'],
-                title=title,
-                comments=[content]
-            )
-            # send to ajax
-            return jsonify(content)
+                # then update the comment section
+                comments.update({'title':title},
+                                {
+                                '$push':{
+                                    'comments':content
+                                }
+                })
+                # send to ajax
+                return jsonify(content)
+            else:
+                # get data
+                content = {
+                        'username':request.args.get('user'),
+                        'date':request.args.get('today'),
+                        'comment':request.args.get('comment')
+                    }
+                # insert to the first comment to the database
+                global_database.insert(ids['comments'],
+                    title=title,
+                    comments=[content]
+                )
+                # send to ajax
+                return jsonify(content)
+    else:
+        flash("sign in to leave a comment")
 
     return render_template('pages/detail.html',
                            repo=repo,
